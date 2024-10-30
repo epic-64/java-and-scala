e2f9b913f3fc13549def201e85fa687270387046
William Raendchen <william.raendchen@easybell.de>
2024-10-31 00:00:00 +0100

    add extension example to App
    GitPrivacy: n3FBUQae+4RTEoi46s+KIZ3+3QB/tDB+2IfxGmsA+DfdgwBOHuU/Vg/S1lV6oRyzBzp3ZURoei8= v5lXqbguPYbyNZSNUOM111WSaIz5IhsislktumpdEIO+6uZkFMwXTXgFR4TKUzpImVEvZXZG9cg=

--- src/main/scala/ScalaPlayground/App.scala
+++ src/main/scala/ScalaPlayground/App.scala
@@ -1,14 +1,26 @@
 package ScalaPlayground
 
 import JavaPlayground.JavaRomans
-import ScalaPlayground.RomanNumerals.ScalaRomansPipeline
+import ScalaPlayground.RomanNumerals.{ScalaRomansImperative, ScalaRomansPipeline}
 
 object App {
+  extension (n: Int)
+    def toRoman: String = (new JavaRomans).toNumeral(n)
+
+  extension (s: String)
+    def fromRoman: Int = (new ScalaRomansImperative).toInt(s)
+  
   def main(args: Array[String]): Unit = {
     val javaRomans = new JavaRomans()
-    println(javaRomans.toNumeral(1996))
-
     val scalaRomans = ScalaRomansPipeline()
+    
+    println(javaRomans.toNumeral(1996))
     println(scalaRomans.toNumeral(1996))
+
+    val numeral147 = 147.toRoman
+    val numberMMXXIV = "MMXXIV".fromRoman
+    
+    println(s"147 is $numeral147")
+    println(s"MMXXIV is $numberMMXXIV")
   }
 }
