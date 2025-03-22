eba7fb0b49d28c53ab335828c8849a17e23d3756
William Raendchen <william.raendchen@gmail.com>
2025-03-23 00:00:00 +0100

    init
    GitPrivacy: KbeiodCkZ51JL2PseCtuVxWYcnUpJ3XoupLZTW+F3qJRQoDaXiyuaGUPk8m0tBaWuPykEdSjw4U= 3ZBl7b7+i+doGMxHDX4PSTulmJlY/5LDDQ17oPUWiU0WJOkRusou7VtP7/mBX5zntFq3/ctyFKA=

--- src/main/scala/ScalaPlayground/HigherKindedTypes/HktSolution/HktSolution.scala
+++ src/main/scala/ScalaPlayground/HigherKindedTypes/HktSolution/HktSolution.scala
@@ -8,27 +8,23 @@ case class Box[A](value: A)
 case class Collection[A](value: Seq[A])
 
 trait PinchEnabler[F[_]]:
-  def map[A, B](fa: F[A])(f: A => B): F[B]
-  def pinch[A](fa: F[A])(f: A => Unit): F[A]
-
-object PinchSyntax:
-  extension [F[_], A](fa: F[A])
-    def map[B](f: A => B)(using enabler: PinchEnabler[F]): F[B] = enabler.map(fa)(f)
-    def pinch(f: A => Unit)(using enabler: PinchEnabler[F]): F[A] = enabler.pinch(fa)(f)
+  extension [A](fa: F[A])
+    def map[B](f: A => B): F[B]
+    def pinch(f: A => Unit): F[A]
 
 object Box:
   given PinchEnabler[Box] with
-    override def map[A, B](fa: Box[A])(f: A => B): Box[B]   = new Box(f(fa.value))
-    override def pinch[A](fa: Box[A])(f: A => Unit): Box[A] = { f(fa.value); fa }
+    extension [A](fa: Box[A])
+      def map[B](f: A => B): Box[B] = Box(f(fa.value))
+      def pinch(f: A => Unit): Box[A] = { f(fa.value); fa }
 
 object Collection:
   given PinchEnabler[Collection] with
-    override def map[A, B](fa: Collection[A])(f: A => B): Collection[B]   = new Collection(fa.value.map(f))
-    override def pinch[A](fa: Collection[A])(f: A => Unit): Collection[A] = { fa.value.foreach(f); fa }
+    extension [A](fa: Collection[A])
+      def map[B](f: A => B): Collection[B] = Collection(fa.value.map(f))
+      def pinch(f: A => Unit): Collection[A] = { fa.value.foreach(f); fa }
 
 @main def run(): Unit =
-  import PinchSyntax.*
-  
   Box(42).map(_ + 1).pinch(println)
   Box("Hello").map(_ + " World").pinch(println)
   Box(User("Alice", 42)).map(user => user.copy(name = user.name.toUpperCase)).pinch(println)
