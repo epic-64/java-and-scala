e61e5afb9271b2e98a3630ec0b1d7102fa231d24
William Raendchen <william.raendchen@gmail.com>
2025-03-23 00:00:00 +0100

    init
    GitPrivacy: bdK1nLzpFK2kvqgYtQ6peVTXA4WkRhtM81q3m6+X1aynouesOU2Cq64bJkQ7m1uDXodDEO3P7Tc= vroZIyAmC3YiiKcvc/YehKGNcA40xqaT5SVFpNFQGncl0Ya/0kT+C3VaBsTIddp8NaiGbMwMoNo=

--- src/main/scala/ScalaPlayground/HigherKindedTypes/HktSolution/HktSolution.scala
+++ src/main/scala/ScalaPlayground/HigherKindedTypes/HktSolution/HktSolution.scala
@@ -1,31 +1,40 @@
 package ScalaPlayground.HigherKindedTypes.HktSolution
 
 case class User(name: String, age: Int)
-case class Box[A](value: A)
-case class Collection[A](value: Seq[A])
+case class MyBox[A](value: A)
+case class MyCollection[A](value: Seq[A])
 
 trait PinchEnabler[F[_]]:
-  extension [A](fa: F[A])
-    def map[B](f: A => B): F[B]
+  extension [A](container: F[A])
+    def transform[B](f: A => B): F[B]
     def pinch(f: A => Unit): F[A]
 
-object Box:
-  given PinchEnabler[Box] with
-    extension [A](fa: Box[A])
-      def map[B](f: A => B): Box[B] = Box(f(fa.value))
-      def pinch(f: A => Unit): Box[A] = { f(fa.value); fa }
+object MyBox:
+  given PinchEnabler[MyBox] with
+    extension [A](container: MyBox[A])
+      def transform[B](f: A => B): MyBox[B] = MyBox(f(container.value))
+      def pinch(f: A => Unit): MyBox[A] = { f(container.value); container }
 
-object Collection:
-  given PinchEnabler[Collection] with
-    extension [A](fa: Collection[A])
-      def map[B](f: A => B): Collection[B] = Collection(fa.value.map(f))
-      def pinch(f: A => Unit): Collection[A] = { fa.value.foreach(f); fa }
+object MyCollection:
+  given PinchEnabler[MyCollection] with
+    extension [A](container: MyCollection[A])
+      def transform[B](f: A => B): MyCollection[B] = MyCollection(container.value.map(f))
+      def pinch(f: A => Unit): MyCollection[A] = { container.value.foreach(f); container }
+
+object MagicLibrary:
+  def doIt[F[_] : PinchEnabler, A, B](container: F[A])(f: A => B): F[B] =
+    container.transform(f).pinch(a => println(s"Pinched: $a"))
 
 @main def run(): Unit =
-  Box(42).map(_ + 1).pinch(println)
-  Box("Hello").map(_ + " World").pinch(println)
-  Box(User("Alice", 42)).map(user => user.copy(name = user.name.toUpperCase)).pinch(println)
+  import MyBox.given
+  import MyCollection.given
   
-  Collection(Seq(1, 2, 3)).map(_ + 1).pinch(println)
-  Collection(Seq("World", "Mars", "Jupiter")).map("Hello " + _).pinch(println)
-  Collection(Seq(User("Alice", 42), User("Bob", 24))).map(user => user.copy(name = user.name.toUpperCase)).pinch(println)
+  val m = MagicLibrary
+  
+  m.doIt(MyBox(42))(_ + 1)
+  m.doIt(MyBox("Hello"))(_ + " World")
+  m.doIt(MyBox(User("Alice", 42)))(user => user.copy(name = user.name.toUpperCase))
+  
+  m.doIt(MyCollection(Seq(1, 2, 3)))(_ + 1)
+  m.doIt(MyCollection(Seq("World", "Mars", "Jupiter")))("Hello " + _)
+  m.doIt(MyCollection(Seq(User("Alice", 42), User("Bob", 24))))(user => user.copy(name = user.name.toUpperCase))
