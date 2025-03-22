a75d61425ac6169c0d8b243cfbb398ce873a6452
William Raendchen <william.raendchen@gmail.com>
2025-03-23 00:00:00 +0100

    cleanup
    GitPrivacy: ZsLXbktE/YEWwZT9uDZ0Sbm4sjL9p1kUmFJWIxi67eh+CmhXkvwXM6ZgDVM/oE0iN4pfUTAWyiM= qskwRmPYXnV6+udvVdtmDNObEoBAagyXKalEDCaTbVkgeTVybNnY2Lb08Z6WntyRW1soY60Rd10=

--- src/main/scala/ScalaPlayground/HigherKindedTypes/HktSolution/HktSolution.scala
+++ src/main/scala/ScalaPlayground/HigherKindedTypes/HktSolution/HktSolution.scala
@@ -16,11 +16,11 @@ object MagicLibrary:
   object OfficialPinchEnablers:
     given PinchEnabler[MyBox] with
       def transform[A, B](box: MyBox[A], f: A => B): MyBox[B] = MyBox(f(box.value))
-      def pinch[A](box: MyBox[A], f: A => Unit): MyBox[A] = { f(box.value); box}
-  
+      def pinch[A](box: MyBox[A], f: A => Unit): MyBox[A]     = { f(box.value); box }
+
     given PinchEnabler[MyCollection] with
       def transform[A, B](c: MyCollection[A], f: A => B): MyCollection[B] = MyCollection(c.value.map(f))
-      def pinch[A](c: MyCollection[A], f: A => Unit): MyCollection[A] = {c.value.foreach(f); c}
+      def pinch[A](c: MyCollection[A], f: A => Unit): MyCollection[A]     = { c.value.foreach(f); c }
 
 @main def run(): Unit =
   import MagicLibrary.doIt
@@ -33,8 +33,14 @@ object MagicLibrary:
   doIt(MyCollection(List(1, 2, 3)))(_ + 1)
   doIt(MyCollection(List("World", "Mars", "Jupiter")))("Hello " + _)
 
+  val listPincher = new PinchEnabler[List]:
+    def transform[A, B](arr: List[A], f: A => B): List[B] = arr.map(f)
+    def pinch[A](arr: List[A], f: A => Unit): List[A]     = { arr.foreach(f); arr }
+
+  doIt(List("hello", "world"))(_.toUpperCase)(using listPincher)
+
   given PinchEnabler[Seq] with
     def transform[A, B](s: Seq[A], f: A => B): Seq[B] = s.map(f)
-    def pinch[A](s: Seq[A], f: A => Unit): Seq[A] = { s.foreach(f); s }
+    def pinch[A](s: Seq[A], f: A => Unit): Seq[A]     = { s.foreach(f); s }
 
   doIt(Seq(User("Alice", 42), User("Bob", 24)))(user => user.copy(name = user.name.toUpperCase))
